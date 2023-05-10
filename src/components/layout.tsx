1a37209d4a6fa86e852cab1e340d64151e75ba51
Michael Klepac <67074804+mklepac90@users.noreply.github.com>
2023-05-10 09:54:57 -0400

    layout v1

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,6 @@
 import { SiteHeader } from "@/components/site-header"
+import Footer from "./footer"
+import Sidebar from "./sidebar"
 
 interface LayoutProps {
   children: React.ReactNode
@@ -9,9 +11,15 @@ export function Layout({ children }: LayoutProps) {
     <div className="flex flex-col h-screen">
       <SiteHeader />
 
-      <main className="flex-1">
-        {children}
+      <main className="flex flex-row">
+        <Sidebar />
+
+        <div className="flex-grow">
+          {children}
+        </div>
       </main>
+
+      <Footer />
     </div>
   )
 }
